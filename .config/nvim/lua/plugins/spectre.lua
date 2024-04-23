38959ba59912f6e4e8462a6da22305a175eaf48f
Sonny Kjellberg <sonnykjellberg@gmail.com>
2024-04-23 21:15:57 +0200

    change spectre keymap

--- .config/nvim/lua/plugins/spectre.lua
+++ .config/nvim/lua/plugins/spectre.lua
@@ -11,7 +11,7 @@ return {
     vim.keymap.set('v', '<leader>rw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
       desc = "Search current word"
     })
-    vim.keymap.set('n', '<leader>rp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+    vim.keymap.set('n', '<leader>rf', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
       desc = "Search on current file"
     })
   end
