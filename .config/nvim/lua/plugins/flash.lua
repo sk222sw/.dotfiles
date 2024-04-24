f0004897d3c6331036642aa12f17488e8081935d
Sonny Kjellberg <sonnykjellberg@gmail.com>
2024-04-24 21:44:34 +0200

    add treesj

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -4,10 +4,10 @@ return {
   opts = {},
   -- stylua: ignore
   keys = {
-    { "<leader>jj", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "<leader>js", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "<leader>jr", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "<leader>jr", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<leader>fj", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "<leader>fs", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "<leader>fr", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "<leader>fr", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     --{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 }
