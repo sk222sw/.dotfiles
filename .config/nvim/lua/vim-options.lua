737fbb63238d2b7d66c7156ec2041b7ebf0179f7
Sonny Kjellberg <sonnykjellberg@gmail.com>
2024-04-27 20:42:37 +0200

    update vim settings

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -5,3 +5,13 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.cmd("set number")
 vim.opt.signcolumn = "yes" -- keep the gutter open to prevent editor from horizontally jumping
+vim.opt.showmode = false -- shown in lualine
+vim.opt.clipboard = "unnamedplus" -- copy to os clipboard
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 13
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
