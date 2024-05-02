73ac1ed11eacbc944e6d72c3c674f20004913b58
Sonny Kjellberg <sonny.kjellberg@postnord.com>
2024-05-02 14:23:35 +0200

    toggle git blame

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,3 +1,10 @@
 return {
-	"lewis6991/gitsigns.nvim",
+  "lewis6991/gitsigns.nvim",
+  init = function()
+    vim.keymap.set("n", "<leader>Gb", ":Gitsigns toggle_current_line_blame<cr>", { desc = "Toggle line blame" })
+
+    require("which-key").register({
+      ["<leader>G"] = { name = "Git" },
+    })
+  end
 }
