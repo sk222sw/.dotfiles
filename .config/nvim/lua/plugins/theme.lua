77e81a7cca54152b9da289c124345e907f21b612
Sonny Kjellberg <sonnykjellberg@gmail.com>
2024-05-08 12:33:04 +0200

    harpoon

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -6,6 +6,14 @@ return {
 }
 
 -- return {
+--   "ellisonleao/gruvbox.nvim",
+--   priority = 1000,
+--   config = function ()
+--     vim.cmd([[colorscheme gruvbox]])
+--   end,
+-- }
+
+-- return {
 -- 	{
 -- 		"catppuccin/nvim",
 -- 		-- enabled = false,
